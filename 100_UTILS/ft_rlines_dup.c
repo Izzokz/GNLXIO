019d316d94fd3c760feee7b6544816d06442a89c
Kylian Zhen-Claverie <kzhen-cl@2E2.42angouleme.fr>
2024-12-07 15:11:47 +0100

    Just a small condition

--- 100_UTILS/ft_rlines_dup.c
+++ 100_UTILS/ft_rlines_dup.c
@@ -18,6 +18,8 @@ t_rlines	ft_rlines_dup(t_rlines rlines)
 	int			len;
 	int			i;
 
+	if (!rlines)
+		return (NULL);
 	len = ft_rlines_len(rlines);
 	new_lines = gnlxio_ft_calloc(len + 1, sizeof(char *));
 	if (!new_lines)
